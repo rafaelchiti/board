231d1b3dbd9ee0f09ad31bf127b27c5495c6483f
Rafael Chiti <rafaelchiti@gmail.com>
2014-04-04 12:05:16 -0300

    Playing a bit more with the labels colors and shapes

--- webapp/client/board/views/card/labels_selector.jsx
+++ webapp/client/board/views/card/labels_selector.jsx
@@ -11,6 +11,10 @@ var LabelsSelector = React.createComponent({
 
     return (
       <div className={'labelsSelector ' + clazz}>
+        <span className="labelsSelector__label label1"></span>
+        <span className="labelsSelector__label label2"></span>
+        <span className="labelsSelector__label label3"></span>
+        <span className="labelsSelector__label label4"></span>
       </div>
     );
   }
